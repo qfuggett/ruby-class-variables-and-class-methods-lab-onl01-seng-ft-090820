5dd7ba44c4f17f8c9dec7ad28160e762f89e6c26
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 00:49:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,21 @@ class Song
   @@count = 0
   @@artists = [ ]
   @@genres = [ ]
-  @@genre_count
+  @@genre_count = { }
+  @@artist_count = { }
   
+  def initialize
+    @@count += 1
+    
+  end
   
+  def count
+    @@count
+  end
+  
+  def genres
+    
+  end
   
   
 end
